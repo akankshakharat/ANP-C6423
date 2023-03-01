d621f417dfd162285481d58bbbf5051231f8e4b2
Akanksha Kharat <123441344+akankshakharat@users.noreply.github.com>
2023-03-01 22:21:08 +0530

    Add files via upload

---
+++ Practicals/testencapsulation.java
@@ -0,0 +1,16 @@
+class TestEncapsulation
+{
+
+	public static void main(String[] args)
+	{
+		// creating object of class EncapsulationExample
+		EncapsulationExample a=new EncapsulationExample();
+		// object is used to access the methods of a class
+		a.setId(1);
+        a.setAge(21);
+		a.setName("Gaurav");
+		System.out.println("Id:"+a.getId()+"\n"+"Age:"+a.getAge()+"\n"+"Name:"+a.getName());
+
+	}
+
+}
